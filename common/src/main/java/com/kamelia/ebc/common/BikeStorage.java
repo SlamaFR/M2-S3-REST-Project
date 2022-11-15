5d15d4580a468ddc01a728be25e6423e327cae68
ZwenDo <lorris.creantor@gmail.com>
2022-11-15 20:23:20 +0100

    Changed return type

--- common/src/main/java/com/kamelia/ebc/common/BikeStorage.java
+++ common/src/main/java/com/kamelia/ebc/common/BikeStorage.java
@@ -2,14 +2,13 @@ package com.kamelia.ebc.common;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.util.Optional;
 import java.util.UUID;
 
 public interface BikeStorage extends Remote {
 
     RemoteOptional<Bike> findById(UUID bikeId) throws RemoteException;
 
-    Bike save(User owner, UUID token) throws RemoteException;
+    Response<Bike> save(User owner, UUID token) throws RemoteException;
 
     void deleteById(UUID bikeId, User user, UUID token) throws RemoteException;
 
