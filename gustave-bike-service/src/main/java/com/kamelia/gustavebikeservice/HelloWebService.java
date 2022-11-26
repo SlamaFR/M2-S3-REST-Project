63dfadc77e471e8f09c305a707f90854aa701de4
notKamui <teillard.jimmy@outlook.fr>
2022-11-26 14:19:25 +0100

    oe

---
+++ gustave-bike-service/src/main/java/com/kamelia/gustavebikeservice/HelloWebService.java
@@ -0,0 +1,12 @@
+package com.kamelia.gustavebikeservice;
+
+import jakarta.jws.WebMethod;
+import jakarta.jws.WebService;
+
+@WebService
+public class HelloWebService {
+  @WebMethod
+  public String sayHello(String name) {
+    return "Say Hello to " + name;
+  }
+}
