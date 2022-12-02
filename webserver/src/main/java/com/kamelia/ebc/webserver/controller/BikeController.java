9420df42862374f10b887f35ea74955ed2a02cc4
Irwin Madet <irwin.madet@gmail.com>
2022-12-02 23:53:45 +0100

    Changes by lorris
    Co-authored-by: ZwenDo <lorris.creantor@gmail.com>

--- webserver/src/main/java/com/kamelia/ebc/webserver/controller/BikeController.java
+++ webserver/src/main/java/com/kamelia/ebc/webserver/controller/BikeController.java
@@ -1,13 +1,30 @@
 package com.kamelia.ebc.webserver.controller;
 
+import com.kamelia.ebc.webserver.dto.BikeDTO;
+import com.kamelia.ebc.webserver.service.BikeService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.rmi.RemoteException;
+import java.util.Set;
 
 @Controller
+@RequestMapping("/api/bike")
 public class BikeController {
 
     // can you please do the thing
+    private final BikeService bikeService;
 
-    
+    public BikeController(BikeService bikeService) {
+        this.bikeService = bikeService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Set<BikeDTO>> getAllBikes() throws RemoteException {
+        return ResponseEntity.ok(bikeService.getAllBikes());
+    }
+
 
 }
